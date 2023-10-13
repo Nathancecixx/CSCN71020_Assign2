92ed2e8b6614dd7bb46117c308a9e16c0104bf27
BigRedSped-Laptop <98977703+Nathancecixx@users.noreply.github.com>
2023-10-12 22:21:13 -0400

    Wrote a single unit test for both getPerimeter, and getArea functions.
    (Completed Task 1)

--- BCSRec_Test/BCSRec_Test.cpp
+++ BCSRec_Test/BCSRec_Test.cpp
@@ -1,6 +1,9 @@
 #include "pch.h"
 #include "CppUnitTest.h"
 
+extern "C" int getPerimeter(int* length, int* width);
+extern "C" int getArea(int* length, int* width);
+
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 namespace BCSRecTest
@@ -9,8 +12,30 @@ namespace BCSRecTest
 	{
 	public:
 		
-		TEST_METHOD(TestMethod1)
+		TEST_METHOD(getPerimeterTestOne)
 		{
+			//This is testing the getPeramitor function using, 
+			//Length = 6
+			//Width = 9
+			//Perimeter = 30
+			int length = 6;
+			int width = 9;
+			int result = 0;
+			result = getPerimeter(&length, &width);
+			Assert::AreEqual(30, result);
+		}
+
+		TEST_METHOD(getAreaTestOne)
+		{
+			//This is testing the getArea function using,
+			//Length = 6
+			//Width = 9
+			//Perimeter = 30
+			int length = 6;
+			int width = 9;
+			int result = 0;
+			result = getArea(&length, &width);
+			Assert::AreEqual(54, result);
 		}
 	};
 }
